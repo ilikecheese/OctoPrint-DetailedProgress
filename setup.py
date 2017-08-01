160f3531386779a006ab6209cabd7cca795c3978
ilikecheese <ilikecheese@users.noreply.github.com>
2017-08-01 01:11:43 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,30 +4,30 @@
 ### Do not forget to adjust the following variables to your own plugin.
 
 # The plugin's identifier, has to be unique
-plugin_identifier = "detailedprogress"
+plugin_identifier = "EidePrintProgress"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_detailedprogress"
+plugin_package = "octoprint_EidePrintProgress"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint-DetailedProgress"
+plugin_name = "OctoPrint-EidePrintProgress"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.1.4"
+plugin_version = "0.1.4x1"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
 plugin_description = """Displays detailed progress on the LCD screen"""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
-plugin_author = "Dattas Moonchaser"
+plugin_author = "me"
 
 # The plugin's author's mail address.
-plugin_author_email = "dattasmoon@gmail.com"
+plugin_author_email = "me@gmail.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/dattas/OctoPrint-DetailedProgress"
+plugin_url = "https://github.com/ilikecheese/OctoPrint-DetailedProgress"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
